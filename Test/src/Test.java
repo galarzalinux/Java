acf91741a95b1038f11290c044f475cf067b5d9f
1459756 <1459756@22HW020537.SOAM.TCS.com>
2018-04-17 14:58:40 -0500

    Testing HashCode Algorithm

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -39,8 +39,8 @@ public class Test {
 
 import java.util.*;
 
-public class Test {
-
+public class Test {	
+/*
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
@@ -64,4 +64,33 @@ public class Test {
 		System.out.println(""+max);
 		in.close();
 	}
+	*/
+	
+	static String s1="qwer";
+	static String s2;
+	static String s3=s1;
+	
+	public static int hashCode1() {
+		int res=0;
+		int i,len=s2.length();
+		byte[] array=s2.getBytes();
+		for (i = 0; i < len; i++) {
+			res+=(int)array[i]*(Math.pow(31.0, (len-1.0-i)));
+		}
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scanf=new Scanner(System.in);
+		s2=scanf.nextLine();
+		scanf.reset();
+		System.out.println("String 1: "+s1+"\nString 2: "+s2);
+		System.out.println("Hash1: "+s1.hashCode()+"\nHash2: "+hashCode1());
+		System.out.println("equals: "+s1.equals(s2));
+		System.out.println("==: "+(s1==s2));
+		System.out.println("==: "+(s1==s3));
+		scanf.close();
+	}
+	
 }
